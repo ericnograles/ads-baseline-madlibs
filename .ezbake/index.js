20aeee87059b7bb809def427518a7ecbf03ced3f
Eric Nograles <grales@gmail.com>
2017-10-14 10:29:00 -0400

    updates icing

--- .ezbake/index.js
+++ .ezbake/index.js
@@ -35,7 +35,7 @@ module.exports = {
       cmd: ['say', 'how much would could a woodchuck chuck if a woodchuck could chuck wood']
     },
     {
-      description: 'Says a tongue twister',
+      description: 'Tells me the job is done',
       cmd: ['echo', `job's done`]
     }
   ]
