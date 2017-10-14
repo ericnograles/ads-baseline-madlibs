bdbdfe55000a3f914ecab06e91f668250dcddf86
Eric Nograles <grales@gmail.com>
2017-10-14 10:35:15 -0400

    updates recipe icing for ComplexTemplate

--- .ezbake/recipes/ComplexTemplate/icing.js
+++ .ezbake/recipes/ComplexTemplate/icing.js
@@ -4,7 +4,7 @@ module.exports = [
     cmd: ['say', '"how much would could a woodchuck chuck if a woodchuck could chuck wood"']
   },
   {
-    description: 'Says a tongue twister',
+    description: `Says that the job is done`,
     cmd: ['echo', `"job's done"`]
   }
 ];
