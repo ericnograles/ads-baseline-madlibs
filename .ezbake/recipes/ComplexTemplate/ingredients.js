b765e6996f652ae6f50137ee3790cf91b3fb05db
Eric Nograles <grales@gmail.com>
2017-10-17 09:39:20 -0400

    removes all fileName refs

--- .ezbake/recipes/ComplexTemplate/ingredients.js
+++ .ezbake/recipes/ComplexTemplate/ingredients.js
@@ -1,12 +1,6 @@
 module.exports = [
   {
     "type": "input",
-    "name": "fileName",
-    "message": "What would you like this complex template to be called?",
-    "default": "complex_template.html"
-  },
-  {
-    "type": "input",
     "name": "title",
     "message": "What is the title of this webpage",
     "default": "supercalifragilisticexpialidocious"
--- .ezbake/recipes/OtherQuery.js
+++ .ezbake/recipes/OtherQuery.js
@@ -8,12 +8,6 @@ module.exports = {
   ingredients: [
     {
       "type": "input",
-      "name": "fileName",
-      "message": "What would you like this SQL file to be named?",
-      "default": "some_table.sql"
-    },
-    {
-      "type": "input",
       "name": "table",
       "message": "What is the name of the table you would like to query?",
       "default": "some_table"
